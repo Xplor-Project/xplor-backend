475a50a894484bf2b7605d18b6b098c97f67ed6e
13Kirat <141816179+13Kirat@users.noreply.github.com>
2025-11-13 14:00:50 +0530

    cors config

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from routes import assets, health
+from fastapi.middleware.cors import CORSMiddleware # Import CORSMiddleware
 
 app = FastAPI(
     title="3D Editor Asset Manager",
     version="1.0.0",
     description="FastAPI backend for managing 3D assets and previews stored in AWS S3 + MongoDB",
+)
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Include Routers
